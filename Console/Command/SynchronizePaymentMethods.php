037efc098bafa35bad97f4c7193612655c105966
sauliususoniswallee <saulius.usonis@wallee.com>
2023-07-26 11:45:39 +0300

    Release 2.0.5

---
+++ Console/Command/SynchronizePaymentMethods.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * WeArePlanet Magento 2
+ *
+ * This Magento 2 extension enables to process payments with WeArePlanet (https://www.weareplanet.com//).
+ *
+ * @package WeArePlanet_Payment
+ * @author wallee AG (http://www.wallee.com/)
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache Software License (ASL 2.0)
+ */
+namespace WeArePlanet\Payment\Console\Command;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State as AppState;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use WeArePlanet\Payment\Api\PaymentMethodConfigurationManagementInterface;
+
+/**
+ * Command to synchronize the payment methods.
+ */
+class SynchronizePaymentMethods extends Command
+{
+
+    /**
+     *
+     * @var AppState
+     */
+    private $appState;
+
+    /**
+     *
+     * @var PaymentMethodConfigurationManagementInterface
+     */
+    private $paymentMethodConfigurationManagement;
+
+    /**
+     *
+     * @param AppState $appState
+     * @param PaymentMethodConfigurationManagementInterface $paymentMethodConfigurationManagement
+     */
+    public function __construct(AppState $appState,
+        PaymentMethodConfigurationManagementInterface $paymentMethodConfigurationManagement)
+    {
+        parent::__construct();
+        $this->appState = $appState;
+        $this->paymentMethodConfigurationManagement = $paymentMethodConfigurationManagement;
+    }
+
+    protected function configure()
+    {
+        $this->setName('weareplanet:payment-method:synchronize')->setDescription(
+            'Synchronizes the WeArePlanet payment methods.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->appState->setAreaCode(Area::AREA_ADMINHTML);
+        $this->paymentMethodConfigurationManagement->synchronize($output);
+    }
+}
